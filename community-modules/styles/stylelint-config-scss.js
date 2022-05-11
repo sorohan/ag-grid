a80876f841fca191ac519ed57df8b2b53d63e4b5
Bernie Sumption <bernie@berniecode.com>
2022-05-11 18:19:28 +0100

    WIP

--- community-modules/styles/stylelint-config-scss.js
+++ community-modules/styles/stylelint-config-scss.js
@@ -4,34 +4,8 @@ module.exports = {
     ],
     customSyntax: "postcss-scss",
     rules: {
-      "at-rule-no-unknown": [
-        true,
-        {
-          // IMPORTANT: this rule adds all Sass @rules except for @import which we want
-          // to ban in favour of @use
-          "ignoreAtRules": [
-            "at-root",
-            "content",
-            "debug",
-            "each",
-            "else",
-            "else if",
-            "error",
-            "extend",
-            "for",
-            "forward",
-            "function",
-            "if",
-            "include",
-            "media",
-            "mixin",
-            "return",
-            "use",
-            "warn",
-            "while"
-          ]
-        }
-      ],
+      "at-rule-no-unknown": null,
+      "scss/at-rule-no-unknown": true,
       "scss/comment-no-loud": true,
       "scss/no-global-function-names": true,
       "scss/no-duplicate-mixins": true
